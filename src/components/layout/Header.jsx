31dddb59cbb1bd9a61106ec7c00a99fc6f1e7efc
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-07-09 03:43:05 +0200

    step 2

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header className="flex items-center w-full justify-around py-4 bg-white">
-      <h1 className="text-[#1D3B4A] font-bold text-[1.5rem]">Acookware</h1>
+    <header className="flex items-center w-full justify-around py-4 bg-white  max-sm:flex max-sm:flex-col max-md:flex max-md:flex-col">
+      <h1 className="text-[#1D3B4A] font-bold text-[1.5rem]  max-sm:hidden max-md:hidden">
+        Acookware
+      </h1>
       <nav>
-        <ul className="flex items-center flex-wrap gap-8 *:font-semibold *:text-[#B5B5B5]">
+        <ul className="flex items-center flex-wrap gap-8 *:font-semibold *:text-[#B5B5B5] max-sm:flex max-sm:flex-col max-md:flex max-md:flex-col max-sm:hidden max-md:hidden">
           <Link
             to={"/"}
             className="hover:border-b-2 hover:border-[#3A83A1] hover:text-[#3A83A1]">
@@ -24,13 +26,21 @@ const Header = () => {
         </ul>
       </nav>
       <form action="#" className="flex">
-        <label htmlFor="search" className="border-b-2 bottom-3 flex gap-2">
+        <label
+          htmlFor="search"
+          className="border-b-2 bottom-3 flex gap-2  max-sm:hidden max-md:hidden">
           <img
             src="search-icon.png"
             alt="Search icon"
             className="object-contain"
           />
-          <input type="text" name="search" id="search" placeholder="Search" />
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+            className=" max-sm:hidden max-md:hidden"
+          />
         </label>
       </form>
       <div className="flex gap-2">
